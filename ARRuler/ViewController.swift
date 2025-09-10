13d1dc5449bacb9dc2b8b1368f6a4e22d07e9dea
Dika <al.ramdhan312@gmail.com>
2025-09-10 16:21:39 +0700

    Initial Commit

---
+++ ARRuler/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  ARRuler
+//
+//  Created by SKK Staf on 10/09/25.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    var dotNodes = [SCNNode]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first {
+                let dotNode = addDot(at: hitResult)
+                dotNodes.append(dotNode)
+                
+                if dotNodes.count >= 2 {
+                    calculate()
+                }
+            }
+        }
+    }
+    
+    func addDot(at location: ARHitTestResult) -> SCNNode {
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        dotNode.position = SCNVector3(
+            x: location.worldTransform.columns.3.x,
+            y: location.worldTransform.columns.3.y,
+            z: location.worldTransform.columns.3.z
+        )
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        return dotNode
+    }
+    
+    func calculate() {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        let distance = sqrt(
+            pow(end.position.x - start.position.x, 2) +
+            pow(end.position.y - start.position.y, 2) +
+            pow(end.position.z - start.position.z, 2)
+        )
+        print(abs(distance))
+        
+        // distance = √ ((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+![banner](banner.png)
+
+# AR Ruler
+
+Course Udemy Section 28: Advanced ARKit - Measuring aplikasi
+
+## Screenshot
